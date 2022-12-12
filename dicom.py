46632d7aba33217d8ad8d2a5e7026b9994f02075
fara <faranak.kheirandish@gmail.com>
2022-12-12 14:16:26 -0500

    define dicom.py

---
+++ dicom.py
@@ -0,0 +1,16 @@
+import pydicom
+import cv2
+import glob
+import os
+import numpy as np
+
+def load_dicom(path, image_sizes):
+    dicom = pydicom.read_file(path)
+    data = dicom.pixel_array
+    data =cv2.resize(data, (image_sizes[0], image_sizes[0]), interpolation=cv2.INTER_LINEAR)
+    return data
+
+def load_dicom_line_par(path):
+    t_paths = sorted(glob(os.path.join(path, "*")), key=lambda x:int(x.split('/')[-1].split('.')[0]))
+    n_scans = len(t_paths)
+    #indices = np.quantile(list(range(n_scans)), np.linspace())
