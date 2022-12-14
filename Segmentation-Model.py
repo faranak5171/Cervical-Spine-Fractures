bb2da614ae48fc1fdcb4d1f812726653b93b769a
fara <faranak.kheirandish@gmail.com>
2022-12-14 12:41:22 -0500

    define decoder

--- Segmentation-Model.py
+++ Segmentation-Model.py
@@ -1,12 +1,17 @@
 import torch.nn as nn
 import timm
 import torch
-
+import segmentation_models_pytorch as smp
 
 class SegModel(nn.Module):
-    def __init__(self, pre_trained_model, drop_rate, drop_path_rate,pretrained=False):
+    def __init__(self, pre_trained_model, drop_rate, drop_path_rate, segmentation_type='unet',pretrained=False):
         super(SegModel, self).__init__()
-
+        
+        # A number of stages used in encoder in range [3, 5]
+        self.encoder_depth = 5
+        # Building an encoder using Timm
+        # create a model for our encoder
+        # features_only = True  --> returns the tensors of all layers
         self.encoder = timm.create_model(
             pre_trained_model,
             pretrained=pretrained,
@@ -15,12 +20,20 @@ class SegModel(nn.Module):
             drop_rate = drop_rate,
             drop_path_rate = drop_path_rate
         )
-
-        # Do feature extraction from the penultimate layer (pre-classifier layer)
-        # Generate a batch of pictures, 1 picture with 3 channels and 64*64 resolution
+        # Generate a picture with 3 channels and 64*64 resolution
         tensor_obj = torch.rand(1,3,64,64)
-        output = self.encoder(tensor_obj) # model(input) to get the output
-        encoder_channels = [1] + [layer.shape[1] for layer in output]
-        print(self.encoder.feature_info)
+        # Extract features of encoder
+        output = self.encoder(tensor_obj) 
+        # get input channels of encoder
+        encoder_channels = [tens.shape[1] for tens in output]
+        # Lenght of the decoder channel should be the same as **encoder_depth**
+        decoder_channels = [256,128,64,32,16]
+        if segmentation_type == 'unet':
+            self.decoder = smp.decoders.unet.decoder.UnetDecoder(
+                encoder_channels = encoder_channels,
+                decoder_channels = decoder_channels,
+                n_blocks = self.encoder_depth
+            )
 
 model = SegModel('resnet18', 0., 0.)
+print(model.decoder)
