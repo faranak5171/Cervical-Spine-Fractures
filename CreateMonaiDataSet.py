fc5dab0897cf400eef33cb54c59a51c8ee1270ee
fara <faranak.kheirandish@gmail.com>
2022-12-20 12:49:18 -0500

    define load dataset

--- CreateMonaiDataSet.py
+++ CreateMonaiDataSet.py
@@ -1,8 +1,22 @@
 from torch.utils.data import Dataset
+import dicom
 
 
 class CreateMonaiDataset(Dataset):
-    def __init__(self, df, dataset_type, transform):
+    def __init__(self, df, labels, transform):
         self.df = df.reset_index()
-        self.ds_type = dataset_type
+        self.labels = labels
         self.transform = transform
+
+    # This function just returns the length of the labels when called
+    def __len__(self):
+        return self.df.shape[0]
+
+    '''
+        This function is used by Pytorch’s Dataset module to get a sample and construct the dataset. When initialised, it will loop through this function creating a sample from each instance in the dataset.
+        index parameter: passed in to the function is a number, this number is the data instance which Dataset will be looping through
+    '''
+
+    def __getitem__(self, index):
+        ds_row = self.df.iloc[index]
+        return dicom.load_train_samples_dicom(ds_row)
